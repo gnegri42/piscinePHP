110c6fc6caab175b1ce9a501b0172e6907fa504a
Geoffrey NEGRI <gnegri@e3r13p8.42.fr>
2018-03-27 19:12:16 +0200

    Modif ex09 to compare every char

--- d01/ex09/ssap2.php
+++ d01/ex09/ssap2.php
@@ -1,62 +1,52 @@
 #!/usr/bin/php
 <?php
-if ($argc > 1)
+function calc_ascii($c)
 {
-	unset($argv[0]);
-	foreach ($argv as $elem)
-	{
-		$tmp = array_filter(explode(" ", $elem));
-		foreach ($tmp as $elem2)
-			$array[] = $elem2;
-	}
-	foreach ($array as $elem)
-	{
-		if (is_numeric($elem) == true || $elem == '0')
-			$arraynum[] = $elem;
-		else if (($elem[0] >= 'A' && $elem[0] <= 'Z') || ($elem[0] >= 'a' && $elem[0] <= 'z'))
-			$arrayalpha[] = $elem;
-		else
-			$array2[] = $elem;
-	}
-	if ($arrayalpha != NULL)
-		natcasesort($arrayalpha);
-	if ($array2 != NULL)
-		natcasesort($array2);
+	$ascii = ord($c);
+	if (is_numeric($c))
+		$ascii += 300;
+	else if (($c >= 'A' && $c <= 'Z'))
+		$ascii += 32;
+	if ($c < '0' || ($c > 'Z' && $c < 'a') || $c > 'z')
+		$ascii += 600;
+	return ($ascii);
+}
+
+function swapper($str1, $str2)
+{
+	$s_str1_len = strlen($str1);
+	$s_str2_len = strlen($str2);
 	$i = 0;
-	$j = 0;
-	$k = 0;
-	while ($i < count($arraynum) - 1)
+	$tab_str1 = str_split($str1, 1);
+	$tab_str2 = str_split($str2, 1);
+	if ($str1 == $str2)
+		return (0);
+	while ($i < $s_str1_len && $i < $s_str2_len)
 	{
-		$j = $i + 1;
-		while ($j < count($arraynum))
+		$str1_ascii = calc_ascii($tab_str1[$i]);
+		$str2_ascii = calc_ascii($tab_str2[$i]);
+		if ($str1_ascii != $str2_ascii)
 		{
-			$k = 0;
-			while ($arraynum[$i][$k] == $arraynum[$j][$k])
-				$k++;
-			if ($arraynum[$i][$k] > $arraynum[$j][$k])
-			{
-				$temp = $arraynum[$i];
-				$arraynum[$i] = $arraynum[$j];
-				$arraynum[$j] = $temp;
-			}
-			$j++;
+			if ($str1_ascii < $str2_ascii)
+				return (-1);
+			else
+				return (1);
 		}
 		$i++;
 	}
-	if ($arrayalpha != NULL)
-	{
-		foreach ($arrayalpha as $elem)
-			echo "$elem\n";
-	}
-	if ($arraynum != NULL)
-	{
-		foreach ($arraynum as $elem)
-			echo "$elem\n";
-	}
-	if ($array2 != NULL)
-	{
-		foreach ($array2 as $elem)
-			echo "$elem\n";
-	}
 }
+
+if ($argc < 2)
+	exit ();
+unset($argv[0]);
+$array = array();
+foreach ($argv as $elem)
+{
+	$tmp = array_filter(explode(" ", $elem));
+	foreach ($tmp as $elem2)
+		$array[] = $elem2;
+}
+usort($array, "swapper");
+foreach ($array as $elem)
+	echo $elem."\n";
 ?>
